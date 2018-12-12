5d137405c4ba7419392841d7aff7ceb4133d1d8e
Jacob Faulk <jacob5567@gmail.com>
2018-12-11 18:29:21 -0800

    working on dividing the tweet properly

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ Jacob Faulk
 j_faulk@u.pacific.edu
 Created on 12/04/2018
 */
+
+window.addEventListener("load", function()
+{
+
+    var userInput = new Vue({
+        el: "#input",
+        data: {
+            userTweet: ""
+        }
+    })
+
+    var app1 = new Vue({
+        el: '#app-1',
+        data: {
+            message: 'Hello Vue!'
+        }
+    })
+
+})
